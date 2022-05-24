6a44c0c95f9eee4f15eeb26d97db9b515b6c142f
Mohammed shahid <www.mohdshahid303@gmail.com>
2022-05-24 07:54:32 +0530

    uploas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [city, setscity] = useState("enter the city");
+  const [searchcity, setsearchcity] = useState(null);
+
+  const searchfunction = () => {
+    async function fetchapi() {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchcity}&units=metric&appid=bab6cf68f1434dbd49d645f96abac20e`;
+      const response = await fetch(url);
+      const ApiData = await response.json();
+      setscity(ApiData);
+      console.log(city);
+    }
+    fetchapi();
+  };
+
+  const finidingcity = (event) => {
+    setsearchcity(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mt-1">
+        <div className="row justify-content-center " id="maincontainer">
+          <div className="col-md-6">
+            <center>
+              <h1 style={{ textTransform: "uppercase" }}>weather forecast</h1>
+            </center>
+          </div>
+          <div className="w-100 m-3"></div>
+
+          <div className="col-md-6" id="mainbox">
+            <nav className="navbar">
+              <form className="d-flex w-100" role="search">
+                <input
+                  className="form-control  me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  onChange={finidingcity}
+                />
+                <button
+                  className="btn btn-danger w-25"
+                  type="button"
+                  onClick={searchfunction}
+                >
+                  Search
+                </button>
+              </form>
+            </nav>
+          </div>
+
+          <div className="w-100 m-3"></div>
+
+          <div className="col-md-6 pt-5 pb-5 text-light" id="mainbox">
+              {!city.name ? (
+                <center>
+                <h1>{city ==="enter the city"? city: "no data"}</h1>
+                </center>
+              ) : (
+                <>
+                  <h1>
+                    <i className="fas fa-street-view"></i>
+                    {city.name}
+                  </h1>
+
+                  <div id="maintemp" className="mt-5">
+                  <center>
+                    <h2>
+                      {city.main.temp}
+                      <sup> o </sup>Cel
+                    </h2>
+
+                    <h3>Min: {city.main.temp_min} | Max: {city.main.temp_max}</h3>
+
+                    <h3>Climate:{city.weather.id}</h3>
+                </center>
+                  </div>
+                  </>
+              )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
